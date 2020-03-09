36962aa255440d5e3348873bcadd746fb70b94a3
jpeart03 <james.r.peart@gmail.com>
2020-03-09 09:47:12 -0500

    initial commit

---
+++ RecycleBlazorServer/Models/ContactModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecycleBlazorServer.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Please use a valid email address")]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(3, ErrorMessage = "Your message must be atleast 3 characters long")]
+        public string Message { get; set; }
+    }
+}
